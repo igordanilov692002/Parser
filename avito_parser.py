3e98bd556833ba82f4b9a8807406b16605bc6be6
igordanilov692002 <igor.danilov692002@mail.ru>
2023-06-28 21:42:09 +0300

    all parsing

--- avito_parser.py
+++ avito_parser.py
@@ -6,6 +6,8 @@ import requests
 from bs4 import BeautifulSoup
 #from pandas import ExcelWriter
 
+url = "https://www.avito.ru/sankt-peterburg/telefony/mobilnye_telefony/apple/iphone_13-ASgBAgICA0SywA3svcgBtMANzqs5sMENiPw3?cd=1"
+
 def get_html(url, params=None):
     """ получение кода страницы """
     headers = {
@@ -15,4 +17,60 @@ def get_html(url, params=None):
     html = requests.get(url, headers=headers, params=params)
     return html
 
-print(get_html("https://www.avito.ru/sankt-peterburg/telefony/mobilnye_telefony/apple/iphone_13-ASgBAgICA0SywA3svcgBtMANzqs5sMENiPw3?cd=1"))
+def get_pages(html):
+    """ получаем количество страниц """
+    soup = BeautifulSoup(html.text, 'html.parser')
+    # находим кол-во страниц, иначе количество страниц равно 1
+    try:
+        pages = soup.find('span', {'data-marker': 'pagination-button/next'}).previous_element
+    except:
+        pages = 1
+    print('Количество найденных страниц: ', pages)
+    return pages
+
+
+def get_content(html):
+    """ сбор контента со страницы """
+    soup = BeautifulSoup(html.text, 'lxml')
+    blocks = soup.find_all('div', class_=re.compile('iva-item-content'))
+    # сбор данных с страницы
+    data = []
+    for block in blocks:
+        data.append({
+            "Наименование": block.find('h3', class_=re.compile('title-root')).get_text(strip=True),
+            'Цена': block.find('span', class_=re.compile('price-text')).get_text(strip=True).replace('₽', '').replace('\xa0', ''),
+            'Город': block.find('a', class_=re.compile('link-link')).get('href').split('/')[1],
+            'Район': block.find('div', class_=re.compile('geo-root')).get_text(strip=True),
+            'Ссылка': url + block.find('a', class_=re.compile('link-link')).get('href'),
+        })
+    return data
+
+def parse(url):
+    search = input('Введите запрос поиска: ')
+    min_price = input('Введите минимальную стоимость: ')
+    max_price = input('Введите максимальную стоимость: ')
+    html = get_html(url, params={'bt': 1, 'pmax': max_price, 'pmin': min_price, 'q': search, 's': '2', 'view': 'gallery'})
+    soup = BeautifulSoup(html.text, 'lxml')
+    print(soup.h1.get_text())
+    print('Ссылка со всеми параметрами:\n', html.url)
+    print('Статус код сайта: ', html.status_code)
+    data = []
+    # проверка сайта на доступ
+    if html.status_code == 200:
+        # вызов функции для вывода количества найденных страниц
+        get_pages(html)
+        pagination = int(input('Сколько страниц спарсить? '))
+        for page in range(1, pagination + 1):
+            html = get_html(url, params={'bt': 1, 'p': page, 'pmax': max_price, 'pmin': min_price, 'q': search, 's': '2', 'view': 'gallery'})
+            print(f'Парсинг страницы {page} из {pagination}...')
+            data.extend((get_content(html)))
+            time.sleep(random.randint(1, 3))
+        print(f'Получили {len(data)} позиций')
+        # сохраняем в эксель, передав наши собранные данные и запрос
+        # save_excel(data, search)
+        print(data)
+    else:
+        print('Ошибка доступа к сайту')
+
+if __name__ == "__main__":
+     parse(url)
